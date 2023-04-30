4b2942d2f73fee6c5c24eb2a95fad9104d4499e7
Gabriel Terra <gabrielterra055@gmail.com>
2023-04-30 18:58:18 -0300

    funcionalidade finalizada
    remoção dos 2 diagramas desatualizados, remoção de atributos inutilizados, criação de testes para novos atributos da classe tictactoe adicionados, função basicas das views.

--- controller/controller_tictactoe.py
+++ controller/controller_tictactoe.py
@@ -1,11 +1,13 @@
+import os
+
 from model.tictactoe import TicTacToe
-from view.tela_principal import *
+from view.tela_tic_tac_toe import TelaTicTacToe
 
 
 class ControllerTicTacToe:
     def __init__(self, controller_principal):
         self.__controller_principal = controller_principal
-        self.__tela_tictactoe = None
+        self.__tela_tictactoe = TelaTicTacToe()
         self.__tic_tac_toe = TicTacToe()
 
     @property
@@ -20,31 +22,33 @@ class ControllerTicTacToe:
         while not self.conferir_fim_da_partida(self.tic_tac_toe.cerquilha, self.tic_tac_toe.jogada):
             jogador = self.definir_jogador()
             self.jogar(jogador)
-        
+
         # se tiver acabado, deve-se ...
-        
+
         ganhador = self.tic_tac_toe.ganhador
         if ganhador != 'V':
             self.tela_tictactoe.mostra_ganhador(self.tic_tac_toe.ganhador)
         else:
-            self.tela_tictactoe.mostra_empate(self.tic_tac_toe.ganhador)
+            self.tela_tictactoe.mostra_empate()
+
+        self.tic_tac_toe.ganhador = None
+        self.tic_tac_toe.jogada = 0
+        self.tic_tac_toe.jogador = 1
+        self.tic_tac_toe.cerquilha = [
+            [0 for j in range(3)] for i in range(3)]
         return
 
-
-
-
-# ================================== J O G A D A =========================================
     def jogar(self, jogador):
         tabela = self.tic_tac_toe.cerquilha
-        self.tela_tictactoe.mostra_tabela(tabela)
-        posicao = self.tela_tictactoe.recolhe_jogada()
+        self.mostra_tabela(tabela)
         
         while True:
+            posicao = self.tela_tictactoe.recolhe_jogada()
             posicao = self.verificar_posicao(posicao, tabela)
             posicao_real = self.converter_em_posicao_real(posicao)
             validade_da_posicao = self.verificar_se_posicao_ja_esta_ocupada(
                 posicao_real, tabela)
-            
+
             if not validade_da_posicao:
                 break
 
@@ -52,7 +56,7 @@ class ControllerTicTacToe:
 
         self.troca_jogador()
         self.acrescentar_jogada()
-        
+        os.system('clear') or None
 
     def converter_em_posicao_real(self, jogada):
         jogada = int(jogada)
@@ -63,11 +67,11 @@ class ControllerTicTacToe:
             return valor
 
         elif jogada <= 6:
-            valor = str(jogada - 6)
+            valor = str(jogada + 6)
             return valor
 
         elif jogada <= 9:
-            valor = str(jogada - 13)
+            valor = str(jogada + 13)
             return valor
 
         else:
@@ -75,25 +79,27 @@ class ControllerTicTacToe:
 
     def verificar_posicao(self, posicao, tabela):
 
-        while posicao < 1 or posicao > 9:
+        while int(posicao) < 1 or int(posicao) > 9:
             self.tela_tictactoe.opcao_invalida()
             posicao = self.tela_tictactoe.jogada(tabela)
 
         return posicao
 
-    def verificar_se_posicao_ja_esta_ocupada(self, posicao, tabela):        
-        return tabela[int(posicao[0])][int(posicao[1])] == 'X' or tabela[posicao[0]][posicao[1]] == 'O'
-# =======================================================================================================
+    def verificar_se_posicao_ja_esta_ocupada(self, posicao, tabela):
+        return tabela[int(posicao[0])][int(posicao[1])] == 'X' or tabela[int(posicao[0])][int(posicao[1])] == 'O'
+
+    def mostra_tabela(self, tabela):
+        self.tela_tictactoe.mostra_tabela(tabela)
 
     def jogada_maior_que_5(self, jogada):
-        return jogada >= 5
+        return jogada >= 4
 
     def troca_jogador(self):
         self.tic_tac_toe.jogador += 1
-    
+
     def acrescentar_jogada(self):
         self.tic_tac_toe.jogada += 1
-    
+
     def definir_jogador(self):
         if self.tic_tac_toe.jogador % 2 == 1:
             return 'X'
@@ -102,28 +108,32 @@ class ControllerTicTacToe:
     def conferir_fim_da_partida(self, tabela, jogada):
         if self.jogada_maior_que_5(jogada):
             # verificação na horizontal
-            
+
             for i in range(0, 3):
-                if tabela[i][0] == tabela[i][1] and tabela[i][1] == tabela[i][2]:
-                    self.tic_tac_toe.__ganhador = tabela[i][1]
-                    return True
-            
-            #verificação na vertical
-            for i in range(0, 3):
-                if tabela[0][i] == tabela[1][i] and tabela[1][i] == tabela[2][i]:
-                    self.tic_tac_toe.__ganhador = tabela[1][i]
+                if tabela[i][0] == tabela[i][1] and tabela[i][1] == tabela[i][2] and type(tabela[i][1]) != int:
+                    self.tic_tac_toe.ganhador = tabela[i][0]
+                    
                     return True
 
-            if tabela[0][0] == tabela[1][1] and tabela[1][1] == tabela[2][2]:
-                self.tic_tac_toe.__ganhador = tabela[1][1]
+            # verificação na vertical
+            for i in range(0, 3):
+                if tabela[0][i] == tabela[1][i] and tabela[1][i] == tabela[2][i] and type(tabela[1][i]) != int:
+                    self.tic_tac_toe.ganhador = tabela[1][i]
+                    
+                    return True
+
+            if tabela[0][0] == tabela[1][1] and tabela[1][1] == tabela[2][2] and type(tabela[1][1]) != int:
+                self.tic_tac_toe.ganhador = tabela[1][1]
+                
                 return True
-            
-            if tabela[2][0] == tabela[1][1] and tabela[1][1] == tabela[0][2]:
-                self.tic_tac_toe.__ganhador = tabela[1][1]
+
+            if tabela[0][2] == tabela[1][1] and tabela[1][1] == tabela[2][0] and type(tabela[1][1]) != int:
+                self.tic_tac_toe.ganhador = tabela[1][1]
+                
                 return True
-            
-        if jogada == 9:
-            self.tic_tac_toe.__ganhador = 'V'
+
+        if jogada == 8:
+            self.tic_tac_toe.ganhador = 'V'
             return True
 
-        return 
+        return
--- view/tela_tic_tac_toe.py
+++ view/tela_tic_tac_toe.py
@@ -1,12 +1,12 @@
 class TelaTicTacToe:
     def jogada(self, tabela):
         self.mostra_tabela(tabela)
-        posiçõa_de_jogada = self.recolhe_Jogada
+        posiçõa_de_jogada = self.recolhe_jogada()
         return posiçõa_de_jogada
 
     def opcao_invalida(self):
         print(20 * '*')
-        print('POR FAVOR DIGITE UMA OPÇÃO VÁLIDA!')
+        print('POR FAVOR DIGITE UMA VALOR VÁLIDA!')
         print(20 * '*')
 
     def mostra_tabela(self, tabela):
@@ -22,22 +22,19 @@ class TelaTicTacToe:
 
     def recolhe_jogada(self):
         print(20 * '-=')
-        jogada = input(' INSIRA A POSIÇÃO DA JOGADA! ')
+        jogada = input(' INSIRA A POSIÇÃO DA JOGADA: ')
         print(20 * '-=')
         return jogada
-    
+
     def mostra_ganhador(self, resultado):
-        print(20 * '-=')
+        print(20 * '*')
         print(f" {resultado}  F O I  O  V E N C E D O R  D A  R O D A D A !")
-        print(20 * '-=')
+        print(20 * '*')
 
     def mostra_empate(self):
-        print(20 * '-=')
+        print(20 * '#')
         print('-=-=-=-=-=-=-=-=- D E U  V E L H A -=-=-=-=-=-=-=-=-=-=')
-        print(20 * '-=')
+        print(20 * '#')
 
 
-'''tela = TelaTicTacToe()
-tabela = [[0 for j in range(3)] for i in range(3)]
-print(type(tabela[2][1]))
-tela.mostra_tabela(tabela)'''
+
